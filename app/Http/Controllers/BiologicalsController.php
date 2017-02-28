7107e583aeee8df71516e8b122080c88aba617d6
mahitagona <gonamahita05@gmail.com>
2017-02-27 21:43:22 -0600

    Upload download Submit Collapse Requests Traits Controllers

--- app/Http/Controllers/BiologicalsController.php
+++ app/Http/Controllers/BiologicalsController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Biological;
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\Traits\FileUploadTrait;
+
+use App\Http\Requests\StoreBiologicalsRequest;
+
 
 class BiologicalsController extends Controller
 {
+    use FileUploadTrait;
+
     public function index()
     {
         return view('biologicals.index', compact('biologicals'));
     }
-    public function store()
+
+    public function store(StoreBiologicalsRequest $request)
     {
-        return view('biologicals.index', compact('biologicals'));
+        $request = $this->saveFiles($request);
+        Biological::create($request->all());
+
+        return redirect()->route('biologicals.index');
     }
 }
