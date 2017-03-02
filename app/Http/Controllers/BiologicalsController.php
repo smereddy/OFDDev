7af95ffb8490b3b88f0e06d6e35ad1ce99688f71
Shiva Ram Chennapragada <schennapragada@unomaha.edu>
2017-03-01 19:50:27 -0600

    index changes

--- app/Http/Controllers/BiologicalsController.php
+++ app/Http/Controllers/BiologicalsController.php
@@ -26,4 +26,22 @@ class BiologicalsController extends Controller
 
         return redirect()->route('biologicals.index');
     }
+
+    public function edit($id)
+    {
+        $biological = Biological::findOrFail($id);
+        return view('biologicals.edit', compact('biological', ''));
+    }
+
+    public function update(UpdateBiologicalsRequest $request, $id)
+    {
+        $request = $this->saveFiles($request);
+        $biological = Biological::findOrFail($id);
+    }
+
+    public function show($id)
+    {
+        $biological = Biological::findOrFail($id);
+        return view('biologicals.show', compact('biological', ''));
+    }
 }
